ca887b1dc91fb21c5f51c3307d707bc042fbb5e5
Alexandre Silva <asilva1604@outlook.com>
2023-12-30 21:58:30 +0000

    add airline documentation

--- src/Airline.h
+++ src/Airline.h
@@ -7,26 +7,69 @@
 
 #include <string>
 
+/**
+ * @brief Represents an airline with various information such as code, name, callsign, and country.
+ */
 class Airline {
-    std::string code_;
-    std::string name_;
-    std::string callsign_;
-    std::string country_;
-    std::wstring wcode_;
-    std::wstring wname_;
-    std::wstring wcallsign_;
-    std::wstring wcountry_;
+    std::string code_;         /**< The airline code. */
+    std::string name_;         /**< The name of the airline. */
+    std::string callsign_;     /**< The callsign used by the airline. */
+    std::string country_;      /**< The country where the airline is based. */
+    std::wstring wcode_;       /**< The wide string representation of the airline code. */
+    std::wstring wname_;       /**< The wide string representation of the airline name. */
+    std::wstring wcallsign_;   /**< The wide string representation of the airline callsign. */
+    std::wstring wcountry_;    /**< The wide string representation of the airline country. */
+
 public:
+    /**
+     * @brief Constructor for the Airline class.
+     * @param code The airline code.
+     * @param name The name of the airline.
+     * @param callsign The callsign used by the airline.
+     * @param country The country where the airline is based.
+     */
     Airline(const std::string &code, const std::string &name, const std::string &callsign, const std::string &country);
-    const std::string &getCode() const;
+
+    /**
+     * @brief Default constructor for the Airline class.
+     */
     Airline();
+
+    /**
+     * @brief Getter function for the airline code.
+     * @return The airline code.
+     */
+    const std::string &getCode() const;
+
+    /**
+     * @brief Getter function for the wide string representation of the airline code.
+     * @return The wide string representation of the airline code.
+     */
     const std::wstring &getWcode() const;
 
+    /**
+     * @brief Getter function for the wide string representation of the airline name.
+     * @return The wide string representation of the airline name.
+     */
     const std::wstring &getWname() const;
 
+    /**
+     * @brief Getter function for the wide string representation of the airline callsign.
+     * @return The wide string representation of the airline callsign.
+     */
     const std::wstring &getWcallsign() const;
 
+    /**
+     * @brief Getter function for the wide string representation of the airline country.
+     * @return The wide string representation of the airline country.
+     */
     const std::wstring &getWcountry() const;
+
+    /**
+     * @brief Equality comparison operator for the Airline class.
+     * @param other Another Airline object to compare with.
+     * @return True if the two objects are equal, false otherwise.
+     */
     bool operator==(const Airline &other) const;
 };
 
